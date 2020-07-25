a30e6fffc02abf6a7a9f55d2d610fb9fd1668d60
Amedeo Corsi <acorsi@scottishpower.com>
2020-07-25 03:13:12 +0100

    added console

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import DashboardContainer from "./containers/Dashboard/DashboardContainer";
 // comment added
 const App = () => {
+  console.log('hello')
   return (
     // TODO: Replace this with your application code
     <DashboardContainer />
